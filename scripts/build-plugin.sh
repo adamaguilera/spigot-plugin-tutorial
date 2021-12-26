28b2e86a6203392f75c1bf8416073cd12aa07b9a
Adam Aguilera <adamkaguilera@gmail.com>
2021-12-26 11:02:59 -0800

    Added plugins directory if not present

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+# enter plugin directory
 cd ./tutorial || exit
 
+# compile project
 mvn compile
-
+# build package
 mvn clean package
-
 cd ..
 
+# make directory for plugins if it doesn't exist
+mkdir -p ./server/plugins
+
+# move target jar into plugin folder
 mv ./tutorial/target/TutorialPlugin-1.0.jar ./server/plugins/
